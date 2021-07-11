62983d64cbda05336f23e7ab3bf4b00fb27b25f3
Yoshilute <yoshilute.248@gmail.com>
2021-07-11 19:03:23 +0800

    simplify

--- CC11/2DArrayTest.java
+++ CC11/2DArrayTest.java
@@ -3,12 +3,11 @@ class TwoArrayTest
 	public static void main(String args[])
 	{
 		int[][] matrix = {{4,5,1},{2,3,9}};
-		for(int i=0; i<matrix.length; i++)
-		{
-			for(int j=0; j<matrix[i].length; j++)
-			{
-				System.out.println(matrix[i][j]);
+		for(int i=0; i<matrix.length; i++){
+			for(int j=0; j<matrix[i].length; j++){
+				System.out.print(matrix[i][j]);
 			}
+		System.out.println();
 		}
 	}
 }
